f5bc716958824051d7d27a54494a586cbbf90d7f
Teiji IMAHASHI <imahashi@sfc-js.keio.ac.jp>
2020-05-04 19:18:30 +0900

    いろいろ調整2

--- linearfunction/linfunc1.js
+++ linearfunction/linfunc1.js
@@ -8,14 +8,20 @@
 //     };
 
 
-
+const VUESLIDER_ERROR_TYPE = {
+  VALUE: 1,
+  INTERVAL: 2,
+  MIN: 3,
+  MAX: 4,
+  ORDER: 5,
+}
 
 const VueSlider  = window[ 'vue-slider-component' ];
 
 var updatevisiblemain = function() {};
 
 var updatevisible = function() {
-  updatevisiblemain();
+  updatevisiblehook();
 };
 
 var makelineq = function(a, b) {
@@ -53,6 +59,7 @@ let app = new Vue({
     func2b: 2,
     func1: true,
     func2: true,
+    errorMsg: '',
   },
   computed: {
     // get1a: function () {
@@ -61,17 +68,39 @@ let app = new Vue({
     // get1b: function () {
     //   return this.func1b;
     // },
-    v1: function () {
-      return app.func1;
+    v1: function() {
+      return this.func1;
     },
-    v2 : function () {
-      return app.func2;
+    v2: function() {
+      return this.func2;
     },
     getfunc1equation: function() {
       return this.func1 ? "y=" + makelineq(this.func1a, this.func1b) : "";
     },
     getfunc2equation: function() {
       return this.func2 ? "y=" + makelineq(this.func2a, this.func2b) : "";
+    },
+  },
+  methods: {
+    f1: function(x) {
+      return this.func1a * x + this.func1b;
+    },
+    f2: function(x) {
+      return this.func2a * x + this.func2b;
+    },
+    clearErrorMsg: function () {
+      this.errorMsg = '';
+    },
+    error: function (type,msg) {
+      switch(type) {
+        case VUESLIDER_ERROR_TYPE.MIN:
+          break
+        case VUESLIDER_ERROR_TYPE.MAX:
+          break
+        case VUESLIDER_ERROR_TYPE.VALUE:
+          break
+      }
+      this.errorMsg = msg;
     },
   },
   watch: {
@@ -81,27 +110,26 @@ let app = new Vue({
     func2b: updatefunction,
     func1: updatevisible,
     func2: updatevisible,
+  },
+  mounted: function() {
+    f1 = brd.create('functiongraph', [this.f1], {strokeWidth:2});
+    f2 = brd.create('functiongraph', [this.f2], {strokeWidth:2, strokeColor:"red"});
+
+    updatevisiblehook = function () {
+      f1.setAttribute({visible: app.v1});
+      f2.setAttribute({visible: app.v2});
+    }
+
+    updatefunctionhook = function () {
+      // f1.needsUpdate = true;
+      // f2.needsUpdate = true;
+      // f1.update().updateRenderer();
+      // f2.update().updateRenderer();
+      brd.update()
+    }
   }
 });
    
 
-function f1(x) {
-	return app.func1a*x+app.func1b;
-}
-
-function f2(x) {
-	return app.func2a*x+app.func2b;
-}
 
 
-var f1 = brd.create('functiongraph', [f1], {strokeWidth:2, needsUpdate: true});
-
-var f2 = brd.create('functiongraph', [f2], {strokeWidth:2, strokeColor:"red", needsUpdate: true});
-
-var updatevisiblemain = function () {
-  f1.setAttribute({visible: app.func1});
-  f2.setAttribute({visible: app.func2});
-}
-
-var updatefunctionhook = function () {
-}
